dea9027068adaa0f700168b5c2a9a10feac8fde4
MarvinFerraro <m.ferraro@live.fr>
2020-11-18 09:52:22 +0100

    Display of maze with data

--- assets/apps.js
+++ assets/apps.js
@@ -10,10 +10,23 @@ $.getJSON("./data/labyrinthes.json", function(data) {
 
 $(document).ready(function() {
 
+    /******************
+     * Function Clear*
+     ******************/
+    const clear = (elementRemove) => {
+        $(`${elementRemove}`).remove();
+    }
+
+    /******************
+     * Function GetDataWithParam*
+     ******************/
     const getDataChoose = (param1, param2) => {
         return allData[param1][param2];
     }
 
+    /******************
+     * Function Click for prepare the display form*
+     ******************/
     $("#isReady").click(function() {
         isReady();
         $("#formReady").removeClass("d-none");
@@ -28,9 +41,12 @@ $(document).ready(function() {
         }
     }
 
+    /******************
+     * On change of maze choose, for display exemple *
+     ******************/
     $("#mazeChoose").on('change', function() {
 
-        $("#exempleChoose option").remove();
+        clear("#exempleChoose option");
         let exemple = $("#mazeChoose").val();
 
         for (const ex in allData[exemple]) {
@@ -40,6 +56,9 @@ $(document).ready(function() {
         }
     })
 
+    /******************
+     * On change of exemple, display the maze chosen *
+     ******************/
     let mazeSelected;
 
     $("#exempleChoose").on('change', function() {
@@ -49,21 +68,56 @@ $(document).ready(function() {
 
         mazeSelected = getDataChoose(`${size}`, `${exemple}`);
         console.log(mazeSelected);
-        display(size, mazeSelected);
+        clear("#grid-container div");
+        displayMaze(size, mazeSelected);
     })
 
-    const display = (size, mazeSelected) => {
+    /******************
+     * Function for Display the maze *
+     ******************/
+    const displayMaze = (size, maze) => {
 
-        $("#trTable tr").remove();
+        let box = 100;
 
-        let td = `<td></td>`;
+        if (size >= 20) {
+            box = 20;
+        }
 
-        let tr = `<tr>${td.repeat(size)}</tr>`;
+        if (size >= 15) {
+            box = 30;
+        }
 
-        let allRow = tr.repeat(size);
+        if (size >= 10) {
+            box = 50;
+        }
 
-        $("#trTable").append(allRow);
+        document.getElementById(
+            "grid-container"
+        ).style.gridTemplateColumns = `repeat(${size}, ${box}px)`;
+        document.getElementById(
+            "grid-container"
+        ).style.gridTemplateRows = `repeat(${size}, ${box}px)`;
 
+        for (let i = 0; i < maze.length; i++) {
+            let borderstyle = "";
+
+            for (let j = 0; j < maze[i]["walls"].length; j++) {
+                if (maze[i]["walls"][j]) {
+                    borderstyle = borderstyle + "solid ";
+                } else {
+                    borderstyle = borderstyle + "none ";
+                }
+            }
+
+            console.log(borderstyle, "cell n° " + i);
+
+            let element = document.createElement("DIV");
+            element.className = "cell cell_" + i;
+            if (i == maze.length - 1) {
+                element.className = "cell_end";
+            }
+            element.style.borderStyle = borderstyle;
+            document.getElementById("grid-container").appendChild(element);
+        }
     }
-
 });
